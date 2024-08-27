7fd35488d4bf66854f04607d8ef9f277e73df983
Prachi Madhukar Navale <85338427+prachinav@users.noreply.github.com>
2024-08-27 05:34:23 +0530

    Create myfile.py

---
+++ myfile.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+  if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(7, 6))
+print(divide(8, 2))
+print(divide(5, 0))
