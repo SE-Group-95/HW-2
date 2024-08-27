06c182a989d505c1b7142ed80823d376182d3d7c
Prachi Madhukar Navale <85338427+prachinav@users.noreply.github.com>
2024-08-27 00:47:41 +0000

    Completed workspace steps

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HW-1
+
+
+![Python Tests](https://github.com/prachinav/SE-Group-97/actions/workflows/main.yml/badge.svg)
+
+![Python](https://img.shields.io/badge/language-Python-blue)
+![License](https://img.shields.io/badge/license-AGPL--3.0-red)
+![Platform](https://img.shields.io/badge/platform-Linux-brightgreen)
---
+++ myfile.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if a != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(7, 6))
+print(divide(8, 2))
