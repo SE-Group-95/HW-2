11c86a7d5fa024560f425c0ff92244931d0390f3
Mayur Gotmare <47857723+MayurG26@users.noreply.github.com>
2024-09-12 18:21:39 +0000

    Fixed code using pylint

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -1,37 +1,41 @@
+"""
+Module for debugging the merge sort algorithm.
+"""
+
 import rand
 
-
-def mergeSort(arr):
-    if (len(arr) == 1):
+def merge_sort(arr):
+    """Sorts an array using the merge sort algorithm."""
+    if len(arr) == 1:
         return arr
 
     half = len(arr) // 2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
+def recombine(left_arr, right_arr):
+    """Merges two sorted arrays into one sorted array."""
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + i] = right_arr[i]
 
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(left_index, len(left_arr)):
+        merge_arr[right_index + i] = left_arr[i]
 
-    return mergeArr
+    return merge_arr
 
-
-arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
-
-print(arr_out)
+if __name__ == "__main__":
+    test_array = rand.random_array([None] * 20)
+    sorted_array = merge_sort(test_array)
+    print(sorted_array)
--- hw2/rand.py
+++ hw2/rand.py
@@ -1,6 +1,11 @@
+"""
+Module for generating random arrays.
+"""
+
 import secrets
 
 def random_array(arr):
-    for i in range(len(arr)):
-        arr[i] = secrets.randbelow(20) + 1
+    """Fills an array with random integers between 1 and 20."""
+    for index, _ in enumerate(arr):
+        arr[index] = secrets.randbelow(20) + 1
     return arr
